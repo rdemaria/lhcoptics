f9e12333a639c423aae7046116990f1076f19819
Riccardo De Maria <riccardodemaria@gmail.com>
2024-04-13 07:46:52 +0200

    update

--- examples/data/make.py
+++ examples/data/make.py
@@ -7,8 +7,3 @@ inj.model.to_json("lhc.json")
 inj.to_json("opt_inj.json")
 inj=LHCOptics.from_madxfile("model.madx",model='xsuite',sliced=True)
 inj.model.to_json("lhc_sliced.json")
-
-
-import os
-os.system("mv *.json ../data/")
-
