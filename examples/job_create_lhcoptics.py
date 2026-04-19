8c282bda9006d040e6624dfbc2c2728672d38ec1
Riccardo De Maria <riccardodemaria@gmail.com>
2026-04-19 09:11:28 +0200

    update

--- examples/job_create_lhcoptics.py
+++ examples/job_create_lhcoptics.py
@@ -5,20 +5,4 @@ mx.load("data/hllhc/optics.madx")
 
 mm=LHCMadxModel.from_madx_scripts("data/lhc/lhc.seq","data/lhc/optics.madx")
 
-
-for IR in LHCOptics._irs:
-    ir=IR.from_model(mx)
-
-for Arc in LHCOptics._arcs:
-    arc=Arc.from_model(mx)
-
-
-ir1=LHCOptics._irs[0].from_model(mx)
-ir1=LHCOptics._irs[0].from_model(mx)
-
-
-ir1=LHCOptics._irs[0].from_madx_optics("data/lhc/optics.madx")
-ir1=LHCOptics._irs[0].from_madx_optics("data/hllhc/optics.madx")
-
-
 opt=LHCOptics.from_model(mx)
